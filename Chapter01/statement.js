07df387206b223eca117862309d70e5cd0ce00cf
Juhye-Kim <kjh960215@gmail.com>
2021-08-19 22:23:47 +0900

    ch1: 함수 추출하기 (amountFor)

--- Chapter01/statement.js
+++ Chapter01/statement.js
@@ -1,3 +1,26 @@
+// 함수 추출하기
+function amountFor(perf, play) {
+  let thisAmount = 0;
+  switch (play.type) {
+    case "tragedy":
+      thisAmount = 40000;
+      if (perf.audience > 30) {
+        thisAmount += 1000 * (perf.audience - 30);
+      }
+      break;
+    case "comedy":
+      thisAmount = 30000;
+      if (perf.audience > 20) {
+        thisAmount += 10000 + 500 * (perf.audience - 20);
+      }
+      thisAmount += 300 * perf.audience;
+      break;
+    default:
+      throw new Error(`알 수 없는 장르: ${play.type}`);
+  }
+  return thisAmount;
+}
+
 // 공연료 청구서를 출력하는 함수
 function statement(invoice, plays) {
   let totalAmount = 0;
@@ -12,25 +35,8 @@ function statement(invoice, plays) {
 
   for (let perf of invoice[0].performances) {
     const play = plays[perf.playID];
-    let thisAmount = 0;
+    let thisAmount = amountFor(perf, play); // 추출한 함수 활용
 
-    switch (play.type) {
-      case "tragedy":
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy":
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${play.type}`);
-    }
     // 포인트 적립
     volumeCredits += Math.max(perf.audience - 30, 0);
     // 희극 관객 5명마다 추가 포인트 제공
