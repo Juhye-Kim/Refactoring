2754d1d114c3195b0788cf117e7f68524bb58a40
Juhye-Kim <kjh960215@gmail.com>
2021-08-19 22:32:09 +0900

    ch1: 함수 선언 바꾸기

--- Chapter01/statement.js
+++ Chapter01/statement.js
@@ -34,9 +34,9 @@ function statement(invoice, plays) {
   }
 
   // 중첩함수
-  function amountFor(aPerformance, play) {
+  function amountFor(aPerformance) {
     let result = 0;
-    switch (play.type) {
+    switch (playFor(aPerformance).type) {
       case "tragedy":
         result = 40000;
         if (aPerformance.audience > 30) {
@@ -51,7 +51,7 @@ function statement(invoice, plays) {
         result += 300 * aPerformance.audience;
         break;
       default:
-        throw new Error(`알 수 없는 장르: ${play.type}`);
+        throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
     }
     return result;
   }
