169232af398a9d4b281aa11d0bd283ba2ecf72e8
Juhye-Kim <kjh960215@gmail.com>
2021-08-21 18:08:51 +0900

    ch1: 데이터 불변성 유지

--- Chapter01/statement.js
+++ Chapter01/statement.js
@@ -1,8 +1,13 @@
 function statement(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances;
+  statementData.performances = invoice.performances.map(enrichPerformance); // 불변성 유지
   return renderPlainText(statementData, plays);
+
+  function enrichPerformance(aPerformance) {
+    const result = Object.assign({}, aPerformance); // 얕은복사
+    return result;
+  }
 }
 
 function renderPlainText(data, plays) {
