a177bf06008776c870e56734feed08965303450b
Juhye-Kim <kjh960215@gmail.com>
2021-08-21 17:15:22 +0900

    ch1: 함수 추출하기

--- Chapter01/statement.js
+++ Chapter01/statement.js
@@ -1,5 +1,8 @@
-// 공연료 청구서를 출력하는 함수
 function statement(invoice, plays) {
+  return renderPlainText(invoice, plays);
+}
+
+function renderPlainText(invoice, plays) {
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
   for (let perf of invoice.performances) {
@@ -7,8 +10,6 @@ function statement(invoice, plays) {
       perf.audience
     }석\n)`;
   }
-  let totalAmount = totalAmount();
-
   result += `총액: ${usd(totalAmount())}\n`;
   result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
