6d496c6ca0c737bf757e03f0a7991178092f7c8c
Juhye-Kim <kjh960215@gmail.com>
2021-08-21 17:34:52 +0900

    ch1: 내부함수 인자도 중간 데이터구조에서 가져오도록 수정

--- Chapter01/statement.js
+++ Chapter01/statement.js
@@ -1,14 +1,14 @@
 function statement(invoice, plays) {
   const statementData = {};
-  statementData.customer = invoice.customer; // 고객데이터를 중간데이터로 옮김
-  return renderPlainText(statementData, invoice, plays);
+  statementData.customer = invoice.customer;
+  statementData.performances = invoice.performances;
+  return renderPlainText(statementData, plays);
 }
 
-// 매개변수 삭제
 function renderPlainText(data, plays) {
   let result = `청구 내역 (고객명: ${data.customer})\n`;
 
-  for (let perf of invoice.performances) {
+  for (let perf of data.performances) {
     result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     }석\n)`;
@@ -19,7 +19,7 @@ function renderPlainText(data, plays) {
 
   function totalAmount() {
     let result = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result += amountFor(perf);
     }
     return result;
@@ -27,7 +27,7 @@ function renderPlainText(data, plays) {
 
   function totalVolumeCredits() {
     let result = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result += volumeCreditsFor(perf);
     }
     return result;
