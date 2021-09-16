9c90749fc33b8808bb48542b11c5ac5c7ddf440e
Juhye-Kim <kjh960215@gmail.com>
2021-09-17 01:27:29 +0900

    ch06-3: ex1 변수 추출하기

--- Chapter06/6.3_변수추출하기/ex1.js
+++ Chapter06/6.3_변수추출하기/ex1.js
@@ -1,5 +1,6 @@
-return (
-  ondragover.quantity * ondragover.itemPrice -
-  Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
-  Math.min(order.quantity * order.itemPrice * 0.1, 100)
-);
+const basePrice = order.quantity * order.itemPrice;
+const quantityDiscount =
+  Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+const shipping = Math.min(order.quantity * order.itemPrice * 0.1, 100);
+
+return basePrice - quantityDiscount + shipping;
