25632451de7e446a9b0cee4424d69b7b2999d69e
Juhye-Kim <kjh960215@gmail.com>
2021-09-17 01:37:00 +0900

    ch06-3: ex2 클래스에서 변수 추출

--- Chapter06/6.3_변수추출하기/ex2.js
+++ Chapter06/6.3_변수추출하기/ex2.js
@@ -12,10 +12,18 @@ class Order {
   }
 
   get price() {
-    return (
-      this.quantity * this.itemPrice -
-      Math.max(0, this.quantity - 500) * this.itemPrice * 0.05 +
-      Math.min(this.quantity * this.itemPrice * 0.1, 100)
-    );
+    return this.basePrice - this.quantityDiscount + this.shipping;
+  }
+
+  get basePrice() {
+    return this.quantity * this.itemPrice;
+  }
+
+  get quantityDiscount() {
+    return Math.max(0, this.quantity - 500) * this.itemPrice * 0.05;
+  }
+
+  get shipping() {
+    return Math.min(this.quantity * this.itemPrice * 0.1, 100);
   }
 }
