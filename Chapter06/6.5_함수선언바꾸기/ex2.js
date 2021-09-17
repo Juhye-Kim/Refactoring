bec8752c9c567ff21b0b686619498870c8af22a1
Juhye-Kim <kjh960215@gmail.com>
2021-09-18 01:32:23 +0900

    ch06-3: ex2 마이그레이션 - 3) 함수 인라인

--- Chapter06/6.5_함수선언바꾸기/ex2.js
+++ Chapter06/6.5_함수선언바꾸기/ex2.js
@@ -4,11 +4,6 @@ class Book {
   }
 
   addReservation(customer) {
-    this.zz_addReservations(customer, false);
-  }
-
-  zz_addReservations(customer, isPriority) {
-    // 새로 추가한 매개변수 실제 사용하는지 확인
     assert(isPriority === true || isPriority === false);
     this._reservations.push(customer);
   }
