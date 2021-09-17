1e5e335964d93868800e6ac9e34fd527773988d3
Juhye-Kim <kjh960215@gmail.com>
2021-09-18 01:31:50 +0900

    ch06-3: ex2 마이그레이션 - 2) 매개변수 추가

--- Chapter06/6.5_함수선언바꾸기/ex2.js
+++ Chapter06/6.5_함수선언바꾸기/ex2.js
@@ -4,10 +4,12 @@ class Book {
   }
 
   addReservation(customer) {
-    this.zz_addReservations(customer);
+    this.zz_addReservations(customer, false);
   }
 
-  zz_addReservations(customer) {
+  zz_addReservations(customer, isPriority) {
+    // 새로 추가한 매개변수 실제 사용하는지 확인
+    assert(isPriority === true || isPriority === false);
     this._reservations.push(customer);
   }
 }
