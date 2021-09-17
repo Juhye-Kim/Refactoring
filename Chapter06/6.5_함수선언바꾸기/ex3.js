0b8957d2bcfd939d1d25b892aa1f8976a123852d
Juhye-Kim <kjh960215@gmail.com>
2021-09-18 01:36:27 +0900

    ch06-3: ex3 매개변수를 속성으로 바꾸기 - 1) 매개변수로 쓸 코드를 변수로 추출

--- Chapter06/6.5_함수선언바꾸기/ex3.js
+++ Chapter06/6.5_함수선언바꾸기/ex3.js
@@ -1,5 +1,6 @@
 function inNewEngland(aCustomer) {
-  return ["MA", "CT", "ME", "VT", "NH", "RI"].includes(aCustomer.address.state);
+  const stateCode = aCustomer.address.state;
+  return ["MA", "CT", "ME", "VT", "NH", "RI"].includes(stateCode);
 }
 
 const newEnglanders = someCustomers.filter((c) => inNewEngland(c));
