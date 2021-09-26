ac5d1cdd3ff7fb0b0570e2e706aa25a07b3c5035
Juhye-Kim <kjh960215@gmail.com>
2021-09-26 23:51:38 +0900

    ch06-8: ex1 before

---
+++ Chapter06/6.8_매개변수객체만들기/ex1.js
@@ -0,0 +1,25 @@
+// 온도 측정값 배열에서 정상 작동 범위를 벗어난 것이 있는지 검사
+
+// 온도 측정값 데이터
+const station = {
+  name: "ZB1",
+  readings: [
+    { temp: 47, time: "2016-11-10 09:10" },
+    { temp: 53, time: "2016-11-10 09:20" },
+    { temp: 58, time: "2016-11-10 09:30" },
+    { temp: 53, time: "2016-11-10 09:40" },
+    { temp: 51, time: "2016-11-10 09:50" },
+  ],
+};
+
+// 정상범위 벗어난 측정값 찾는 함수
+function readingsOutsideRange(station, min, max) {
+  return station.readings.filter((r) => r.temp < min || r.temp > max);
+}
+
+// 호출문
+alerts = readingsOutsideRange(
+  station,
+  operatingPlan.temperatureFloor,
+  operatingPlan.temperatureCeiling
+);
