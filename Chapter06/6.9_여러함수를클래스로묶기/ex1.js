d61eb97a654a0e47b76f865725c15fd6022f084d
Juhye-Kim <kjh960215@gmail.com>
2021-10-03 01:45:16 +0900

    ch06-9: ex1 레코드 캡슐화

--- Chapter06/6.9_여러함수를클래스로묶기/ex1.js
+++ Chapter06/6.9_여러함수를클래스로묶기/ex1.js
@@ -18,3 +18,29 @@ function calculateBaseCharge(aReading) {
   // 기본 요금 계산 함수
   return baseRate(aReading.month, aReading.year) * aReading.qauantity;
 }
+
+// 캡슐화
+class Reading {
+  constructor(data) {
+    this._customer = data.customer;
+    this._quantity = data.qauantity;
+    this._month = data.month;
+    this._year = data.year;
+  }
+
+  get customer() {
+    return this._customer;
+  }
+
+  get qauantity() {
+    return this._quantity;
+  }
+
+  get month() {
+    return this._month;
+  }
+
+  get year() {
+    return this._year;
+  }
+}
