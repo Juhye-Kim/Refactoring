e50d2bced4700d49f2269fcf87e009688a13d26e
Juhye-Kim <kjh960215@gmail.com>
2021-11-29 18:05:15 +0900

    ch06-11 ex1 배송비 계산부분 함수 추출

--- Chapter06/6.11_단계쪼개기/ex1.js
+++ Chapter06/6.11_단계쪼개기/ex1.js
@@ -6,8 +6,12 @@ function priceOrder(product, quantity, shippingMethod) {
     Math.max(quantity - product.discountThreshold, 0) *
     product.basePrice *
     product.discountRate;
+  const price = applyShipping(basePrice, shippingMethod, quantity, discount);
+  return price;
+}
 
-  // 배송정보를 이용해 배송비 계산
+// 배송정보를 이용해 배송비 계산
+function applyShipping(basePrice, shippingMethod, quantity, discount) {
   const shippingPerCase =
     basePrice > shippingMethod.discountThreshold
       ? shippingMethod.discountedFee
