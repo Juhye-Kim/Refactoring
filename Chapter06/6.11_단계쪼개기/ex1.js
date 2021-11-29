cefa8680b0f098f4f06d4ad5c9af67d1868a51ed
Juhye-Kim <kjh960215@gmail.com>
2021-11-29 18:10:26 +0900

    ch06-11 ex1 첫번째 단계 코드 함수 추출

--- Chapter06/6.11_단계쪼개기/ex1.js
+++ Chapter06/6.11_단계쪼개기/ex1.js
@@ -1,14 +1,18 @@
 // 상품 결제 금액 계산
 function priceOrder(product, quantity, shippingMethod) {
-  // 상품정보를 이용해 상품가격 계산
+  const priceData = calculatePricingData(product, quantity);
+  const price = applyShipping(priceData, shippingMethod);
+  return price;
+}
+
+// 상품정보를 이용해 상품가격 계산
+function calculatePricingData(product, quantity) {
   const basePrice = product.basePrice * quantity;
   const discount =
     Math.max(quantity - product.discountThreshold, 0) *
     product.basePrice *
     product.discountRate;
-  const priceData = { basePrice, quantity, discount }; // 중간 데이터 구조
-  const price = applyShipping(priceData, shippingMethod);
-  return price;
+  return { basePrice, quantity, discount }; // 중간 데이터 구조
 }
 
 // 배송정보를 이용해 배송비 계산
