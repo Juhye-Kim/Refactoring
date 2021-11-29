9c39fcf35f26d9bd08e2dc2cac11b8afae787bbe
Juhye-Kim <kjh960215@gmail.com>
2021-11-29 18:08:28 +0900

    ch06-11 ex1 중간 데이터로 매개변수 옮기기

--- Chapter06/6.11_단계쪼개기/ex1.js
+++ Chapter06/6.11_단계쪼개기/ex1.js
@@ -6,30 +6,18 @@ function priceOrder(product, quantity, shippingMethod) {
     Math.max(quantity - product.discountThreshold, 0) *
     product.basePrice *
     product.discountRate;
-  const priceData = {}; // 중간 데이터 구조
-  const price = applyShipping(
-    priceData,
-    basePrice,
-    shippingMethod,
-    quantity,
-    discount
-  );
+  const priceData = { basePrice, quantity, discount }; // 중간 데이터 구조
+  const price = applyShipping(priceData, shippingMethod);
   return price;
 }
 
 // 배송정보를 이용해 배송비 계산
-function applyShipping(
-  priceData,
-  basePrice,
-  shippingMethod,
-  quantity,
-  discount
-) {
+function applyShipping(priceData, shippingMethod) {
   const shippingPerCase =
-    basePrice > shippingMethod.discountThreshold
+    priceData.basePrice > shippingMethod.discountThreshold
       ? shippingMethod.discountedFee
       : shippingMethod.feePerCase;
-  const shippingCost = quantity * shippingPerCase;
-  const price = basePrice - discount + shippingCost;
+  const shippingCost = priceData.quantity * shippingPerCase;
+  const price = priceData.basePrice - priceData.discount + shippingCost;
   return price;
 }
