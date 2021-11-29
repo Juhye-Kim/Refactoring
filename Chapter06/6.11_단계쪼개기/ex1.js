923a3ce2c3908438ca70dbdcba5d3541db4ae891
Juhye-Kim <kjh960215@gmail.com>
2021-11-29 18:03:45 +0900

    ch06-11 ex1 before

--- Chapter06/6.11_단계쪼개기/ex1.js
+++ Chapter06/6.11_단계쪼개기/ex1.js
@@ -1,10 +1,13 @@
 // 상품 결제 금액 계산
 function priceOrder(product, quantity, shippingMethod) {
+  // 상품정보를 이용해 상품가격 계산
   const basePrice = product.basePrice * quantity;
   const discount =
     Math.max(quantity - product.discountThreshold, 0) *
     product.basePrice *
     product.discountRate;
+
+  // 배송정보를 이용해 배송비 계산
   const shippingPerCase =
     basePrice > shippingMethod.discountThreshold
       ? shippingMethod.discountedFee
