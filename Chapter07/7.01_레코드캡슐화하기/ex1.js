e14779390829fca8e9d0d8336d93107cae2e8016
Juhye-Kim <kjh960215@gmail.com>
2021-11-30 19:31:35 +0900

    ch07-1 ex1 레코드 클래스화

--- Chapter07/7.01_레코드캡슐화하기/ex1.js
+++ Chapter07/7.01_레코드캡슐화하기/ex1.js
@@ -1,7 +1,20 @@
-const organization = { name: "애크미 구스베리", country: "GB" };
+const organization = new Organization({
+  name: "애크미 구스베리",
+  country: "GB",
+});
 
 function getRawDataOfOrganization() {
+  return organization._data;
+}
+
+function getOrganization() {
   return organization;
+}
+
+class Organization {
+  constructor(data) {
+    this._data = data;
+  }
 }
 
 result += `<h1>${getRawDataOfOrganization().name}</h1>`; // read
