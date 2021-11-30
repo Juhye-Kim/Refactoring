73fbd05ae027015a8ffe6bdee10ff771c9ad5767
Juhye-Kim <kjh960215@gmail.com>
2021-11-30 19:46:18 +0900

    ch07-1 ex2 before

---
+++ Chapter07/7.01_레코드캡슐화하기/ex2.js
@@ -0,0 +1,30 @@
+const customerData = {
+  1920: {
+    name: "마틴 파울러",
+    id: "1920",
+    usages: {
+      2016: {
+        1: 50,
+        2: 55,
+      },
+      2015: {
+        1: 70,
+        2: 63,
+      },
+    },
+  },
+  38673: {
+    name: "닐 포드",
+    id: "38673",
+  },
+};
+
+// write
+customerData[customerID].usages[year][month] = amount;
+
+// read
+function compareUsage(customerID, laterYear, month) {
+  const later = customerData[customerID].usages[laterYear][month];
+  const earlier = customerData[customerID].usages[laterYear - 1][month];
+  return { laterAmount: later, change: later - earlier };
+}
