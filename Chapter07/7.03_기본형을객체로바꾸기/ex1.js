a79a02b3f9e89ecf091649aab83977b4bd337d09
Juhye-Kim <kjh960215@gmail.com>
2022-01-29 13:58:56 +0900

    ch07-3 ex1 Priority클래스 동작 추가 (더 가다듬기)

--- Chapter07/7.03_기본형을객체로바꾸기/ex1.js
+++ Chapter07/7.03_기본형을객체로바꾸기/ex1.js
@@ -18,15 +18,37 @@ class Order {
 
 class Priority {
   constructor(value) {
-    this._value = value;
+    if (value instanceof Priority) return value;
+    if (Priority.legalValues().includes(value)) this._value = value;
+    else throw new Error(`${value} is invalid for Priority`);
   }
 
   toString() {
     return this._value;
   }
+
+  get _index() {
+    return Priority.legalValues().findIndex((s) => s === this._value);
+  }
+
+  static legalValues() {
+    return ["low", "normal", "high", "rush"];
+  }
+
+  equals(other) {
+    return this._index === other._index;
+  }
+
+  higherThan(other) {
+    return this._index > other._index;
+  }
+
+  lowerThan(other) {
+    return this._index < other._index;
+  }
 }
 
 // client
-const highPriorityCount = orders.filter(
-  (o) => "high" === o.priorityString || "rush" === o.priorityString
-).length;
+const highPriorityCount = orders.filter((o) =>
+  o.priority.higherThan(new Priority("normal"))
+);
