de7cabcfa1cf9493555d180add5556ac537ab6f2
Juhye-Kim <kjh960215@gmail.com>
2022-01-29 13:47:06 +0900

    ch07-3 ex1 변수 캡슐화

--- Chapter07/7.03_기본형을객체로바꾸기/ex1.js
+++ Chapter07/7.03_기본형을객체로바꾸기/ex1.js
@@ -1,6 +1,14 @@
 class Order {
   constructor(data) {
-    this.priority = data.priority;
+    this._priority = data.priority;
+  }
+
+  get priority() {
+    return this._priority;
+  }
+
+  set priority(aString) {
+    this._priority = aString;
   }
 }
 
