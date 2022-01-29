a04640719a20d37213db197cf9c06a1601363a73
Juhye-Kim <kjh960215@gmail.com>
2022-01-29 13:31:27 +0900

    ch07-2 ex1 setter제거 및 add, remove 메서드 추가

--- Chapter07/7.02_컬렉션캡슐화하기/ex1.js
+++ Chapter07/7.02_컬렉션캡슐화하기/ex1.js
@@ -12,8 +12,19 @@ class Person {
     return this._courses;
   }
 
-  set courses(aList) {
-    this._courses = aList;
+  addCourse(aCourse) {
+    this._courses.push(aCourse);
+  }
+
+  removeCourse(
+    aCourse,
+    fnIfAbsent = () => {
+      throw new RangeError();
+    }
+  ) {
+    const index = this._courses.indexOf(aCourse);
+    if (index === -1) fnIfAbsent();
+    else this._courses.splice(index, 1);
   }
 }
 
