840d13dff042828485f8cb603a9ff3d2b386b668
Juhye-Kim <kjh960215@gmail.com>
2022-01-29 14:13:28 +0900

    ch07-4 ex1 before

---
+++ Chapter07/7.04_임시변수를질의함수로바꾸기/ex1.js
@@ -0,0 +1,15 @@
+class Order {
+  constructor(quantity, item) {
+    this._quantity = quantity;
+    this._item = item;
+  }
+
+  get price() {
+    const basePrice = this._quantity * this._itemPrice;
+    let discountFactor = 0.98;
+
+    if (basePrice > 1000) discountFactor -= 0.03;
+
+    return basePrice * discountFactor;
+  }
+}
