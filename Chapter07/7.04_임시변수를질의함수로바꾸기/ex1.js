c4cf995e7baa7f541c24226eb5ea7418aaf39672
Juhye-Kim <kjh960215@gmail.com>
2022-01-29 14:16:01 +0900

    ch07-4 ex1 변수 인라인

--- Chapter07/7.04_임시변수를질의함수로바꾸기/ex1.js
+++ Chapter07/7.04_임시변수를질의함수로바꾸기/ex1.js
@@ -5,12 +5,11 @@ class Order {
   }
 
   get price() {
-    const basePrice = this.basePrice;
     let discountFactor = 0.98;
 
-    if (basePrice > 1000) discountFactor -= 0.03;
+    if (this.basePrice > 1000) discountFactor -= 0.03;
 
-    return basePrice * discountFactor;
+    return this.basePrice * discountFactor;
   }
 
   get basePrice() {
