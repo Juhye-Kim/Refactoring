929c00f1beeebfd5d835e4c200f4faba6b6fced0
Juhye-Kim <kjh960215@gmail.com>
2022-03-11 23:06:57 +0900

    ch07-8 ex1 위임객체를 얻는 getter 추가

---
+++ Chapter07/7.08_중개자제거하기/ex1.js
@@ -0,0 +1,22 @@
+class Person {
+    constructor(name) {
+        this._name = name;
+    }
+
+    get name() {return this._name;}
+    get department() {this._department;}
+    set department(arg) {this._department = arg;}
+    get manager() {this._department.manager;}
+}
+
+class Department {
+    get chargeCode() {return this._chargeCode;}
+    set chargeCode(arg) {this._chargeCode = arg;}
+    get manager() {return this._manager;}
+    set manager(arg) {this._manager = arg;}
+}
+
+// client
+const aPerson = new Person('jooing');
+// ...
+const manager = aPerson.department.manager;
