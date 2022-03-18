308efb8858b40001269ebab810fa4644f77017e9
Juhye-Kim <kjh960215@gmail.com>
2022-03-19 01:07:10 +0900

    ch08-1 ex1 distance, radians 함수 최상위로 이동

--- Chapter08/8.01_함수옮기기/ex1.js
+++ Chapter08/8.01_함수옮기기/ex1.js
@@ -17,17 +17,18 @@ function totalDistance(points) {
         result += distance(points[i-1], points[i]);
     }
     return result;
+}
 
-    function distance(p1, p2) {
-        const EARTH_RADIUS = 3959;
-        const dLat = radians(p2.lat) - radians(p1.lat);
-        const dLon = radians(p2.lon) - radians(p2.lon);
-        const a = Math.pow(Math.sin(dLat / 2), 2) + Math.cos(radians(p2.lat)) * Math.cos(radians(p1.lat)) * Math.pow(Math.sin(dLon / 2), 2);
-        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+function distance(p1, p2) {
+    const EARTH_RADIUS = 3959;
+    const dLat = radians(p2.lat) - radians(p1.lat);
+    const dLon = radians(p2.lon) - radians(p2.lon);
+    const a = Math.pow(Math.sin(dLat / 2), 2) + Math.cos(radians(p2.lat)) * Math.cos(radians(p1.lat)) * Math.pow(Math.sin(dLon / 2), 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
-        return EARTH_RADIUS * c;
-    }
-    function radians(degrees) {
-        return degrees * Math.PI / 180;
-    }
+    return EARTH_RADIUS * c;
+}
+
+function radians(degrees) {
+    return degrees * Math.PI / 180;
 }
