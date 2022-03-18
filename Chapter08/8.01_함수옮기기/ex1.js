d182cb6d873dacbf0e3255ef33ecbbb32b279884
Juhye-Kim <kjh960215@gmail.com>
2022-03-19 00:57:15 +0900

    ch08-1 ex1 calculateDistance 함수 최상위로 이동

--- Chapter08/8.01_함수옮기기/ex1.js
+++ Chapter08/8.01_함수옮기기/ex1.js
@@ -1,6 +1,6 @@
 // GPS 추적 기록의 총 거리를 계산하는 함수
 function trackSummary(points) {
-    const totalTime = calculateTime();
+    const totalTime = calculateTime(points);
     const totalDistance = calculateDistance();
     const pace = totalTime / 60 / totalDistance;
     return {
@@ -9,15 +9,15 @@ function trackSummary(points) {
         pace
     }
 
-    function calculateDistance() {
-        let result = 0;
-        for (let i = 0; i < points.length; i++) {
-            result += distance(points[i-1], points[i]);
-        }
-        return result;
-    }
-
     function distance(p1, p2) {}
     function radians(degrees) {}
     function calculateTime() {}
+}
+
+function top_calculateDistance(points) {
+    let result = 0;
+    for (let i = 0; i < points.length; i++) {
+        result += distance(points[i-1], points[i]);
+    }
+    return result;
 }
