3eeaa9fa3fe75810c3d92d1d0f8ea4c1b8293eed
Juhye-Kim <kjh960215@gmail.com>
2022-03-19 01:00:26 +0900

    ch08-1 ex1 distance, radians 함수 작성

--- Chapter08/8.01_함수옮기기/ex1.js
+++ Chapter08/8.01_함수옮기기/ex1.js
@@ -1,7 +1,7 @@
 // GPS 추적 기록의 총 거리를 계산하는 함수
 function trackSummary(points) {
-    const totalTime = calculateTime(points);
-    const totalDistance = calculateDistance();
+    const totalTime = calculateTime();
+    const totalDistance = calculateDistance(points);
     const pace = totalTime / 60 / totalDistance;
     return {
         time: totalTime,
@@ -9,8 +9,18 @@ function trackSummary(points) {
         pace
     }
 
-    function distance(p1, p2) {}
-    function radians(degrees) {}
+    function distance(p1, p2) {
+        const EARTH_RADIUS = 3959;
+        const dLat = radians(p2.lat) - radians(p1.lat);
+        const dLon = radians(p2.lon) - radians(p2.lon);
+        const a = Math.pow(Math.sin(dLat / 2), 2) + Math.cos(radians(p2.lat)) * Math.cos(radians(p1.lat)) * Math.pow(Math.sin(dLon / 2), 2);
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        return EARTH_RADIUS * c;
+    }
+    function radians(degrees) {
+        return degrees * Math.PI / 180;
+    }
     function calculateTime() {}
 }
 
