4bdb0437c5e1a19c5066bd53eec4cf03a8b520ee
Juhye-Kim <kjh960215@gmail.com>
2022-03-19 13:24:24 +0900

    ch08-2 ex2 접근자가 타깃필드를 사용하도록 수정

--- Chapter08/8.02_필드옮기기/ex2.js
+++ Chapter08/8.02_필드옮기기/ex2.js
@@ -5,12 +5,10 @@ class Account {
     constructor(number, type, interestRate) {
         this._number = number;
         this._type = type;
-        assert(interestRate === this._type.interestRate);
-        this._interestRate = interestRate;
     }
 
     get interestRate() {
-        return this._interestRate;
+        return this._type.interestRate;
     }
 }
 
