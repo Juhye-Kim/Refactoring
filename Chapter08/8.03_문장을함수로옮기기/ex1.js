8ddaab93ef88fdd3b3be4c6defe9b7385f294f95
Juhye-Kim <kjh960215@gmail.com>
2022-03-19 14:02:05 +0900

    ch08-3 ex1 원래 함수 이름 변경 및 제거

--- Chapter08/8.03_문장을함수로옮기기/ex1.js
+++ Chapter08/8.03_문장을함수로옮기기/ex1.js
@@ -6,7 +6,7 @@ function renderPerson(outStream, person) {
 
     result.push(`<p>${person.name}</p>`);
     result.push(renderPhoto(person.photo));
-    result.push(zznew(person.photo));
+    result.push(emitPhotoData(person.photo));
 
     return result.join("\n")
 }
@@ -14,21 +14,12 @@ function renderPerson(outStream, person) {
 function photoDiv(p) {
     return [
         "<div>",
-        zznew(p),
+        emitPhotoData(p),
         "</div>"
     ].join("\n");
 }
 
-function emitPhotoData(aPhoto) {
-    const result = [];
-
-    result.push(`<p>위치: ${aPhoto.location}</p>`);
-    result.push(`<p>날짜: ${aPhoto.date.toDateString()}</p>`);
-
-    return result.join("\n");
-}
-
-function zznew(p) {
+function emitPhotoData(p) {
     return [
         `<p>제목: ${p.title}</p>`,
         `<p>위치: ${aPhoto.location}</p>`
