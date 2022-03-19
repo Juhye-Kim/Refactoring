752625ad0b80d5a147812ca0700347330b096590
Juhye-Kim <kjh960215@gmail.com>
2022-03-19 14:19:13 +0900

    ch08-4 ex1 원래 함수 인라인

--- Chapter08/8.04_문장을호출한곳으로옮기기/ex1.js
+++ Chapter08/8.04_문장을호출한곳으로옮기기/ex1.js
@@ -1,7 +1,8 @@
 function renderPerson(outStream, person) {
     outStream.write(`<p>${person.name}</p>\n`);
     renderPhoto(outStream, person.photo);
-    emitPhotoData(outStream, person.photo);
+    zztmp(outStream, person.photo);
+    outStream.write(`<p>위치: ${photo.location}</p>\n`);
 }
 
 function listRecentPhotos(outStream, photos) {
@@ -9,7 +10,8 @@ function listRecentPhotos(outStream, photos) {
         .filter(p => p.date > recentDateCutoff())
         .forEach(p => {
             outStream.write("<div>\n");
-            emitPhotoData(outStream, p);
+            zztmp(outStream, p);
+            outStream.write(`<p>위치: ${photo.location}</p>\n`);
             outStream.write("</div>\n");
         });
 }
