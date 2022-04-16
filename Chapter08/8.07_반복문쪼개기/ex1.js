aec4a7666bf29450e71ed1c2df71a59ec7f86c7a
Juhye-Kim <kjh960215@gmail.com>
2022-04-16 23:27:34 +0900

    ch08-7 ex1 함수 구분하기

--- Chapter08/8.07_반복문쪼개기/ex1.js
+++ Chapter08/8.07_반복문쪼개기/ex1.js
@@ -1,13 +1,26 @@
-function execute() {
+function totalSalary() {
     let totalSalary = 0;
+
     for (const p of people) {
         totalSalary += p.salary;
     }
-    
+
+    return totalSalary;
+}
+
+function youngestAge() {
     let youngest = people[0] ? people[0].age : Infinity;
+
     for (const p of people) {
         if (p.age < youngest) youngest = p.age;
     }
 
+    return youngest;
+}
+
+function execute() {
+    const youngest = youngestAge();
+    const totalSalary = totalSalary();
+    
     return `최연소: ${youngest}, 총 급여: ${totalSalary}`;
 }
