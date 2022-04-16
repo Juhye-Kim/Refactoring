ff1218a2b298c49342e0443da727951f9af41fa4
Juhye-Kim <kjh960215@gmail.com>
2022-04-16 23:40:08 +0900

    ch08-8 ex1 filter로 대체

--- Chapter08/8.08_반복문을파이프라인으로바꾸기/ex1.js
+++ Chapter08/8.08_반복문을파이프라인으로바꾸기/ex1.js
@@ -1,13 +1,15 @@
 function acquireData(input) {
     const lines = input.split("\n");  // collection
     const result = [];
-    const loopItems = lines.slice(1).filter(line => line.trim() !== "").map(line => line.split(","));
+    const loopItems = lines
+                        .slice(1)
+                        .filter(line => line.trim() !== "")
+                        .map(line => line.split(","))
+                        .filter(record => record[1].trim() === "India");
 
     for (const line of loopItems) {
         const record = line;
-        if (record[1].trim() === "India") {
-            result.push({city: record[0].trim(), phone: record[2].trim()});
-        }
+        result.push({city: record[0].trim(), phone: record[2].trim()});
     }
     return result;
 }
