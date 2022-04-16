7c14793c0872fe5baba6107ddbcaa233edc81295
Juhye-Kim <kjh960215@gmail.com>
2022-04-16 23:41:37 +0900

    ch08-8 ex1 변수 인라인

--- Chapter08/8.08_반복문을파이프라인으로바꾸기/ex1.js
+++ Chapter08/8.08_반복문을파이프라인으로바꾸기/ex1.js
@@ -1,10 +1,9 @@
 function acquireData(input) {
     const lines = input.split("\n");  // collection
-    const result = lines
-                    .slice(1)
-                    .filter(line => line.trim() !== "")
-                    .map(line => line.split(","))
-                    .filter(record => record[1].trim() === "India")
-                    .map(record => ({city: record[0].trim(), phone: record[2].trim()}));
-    return result;
+    return lines
+            .slice(1)
+            .filter(line => line.trim() !== "")
+            .map(line => line.split(","))
+            .filter(record => record[1].trim() === "India")
+            .map(record => ({city: record[0].trim(), phone: record[2].trim()}));
 }
