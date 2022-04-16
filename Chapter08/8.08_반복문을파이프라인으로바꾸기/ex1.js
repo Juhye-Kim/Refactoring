64bb34cf4d84f4d356a6638a5518994203b83bd2
Juhye-Kim <kjh960215@gmail.com>
2022-04-16 23:37:25 +0900

    ch08-8 ex1 slice로 대체

--- Chapter08/8.08_반복문을파이프라인으로바꾸기/ex1.js
+++ Chapter08/8.08_반복문을파이프라인으로바꾸기/ex1.js
@@ -1,14 +1,9 @@
 function acquireData(input) {
     const lines = input.split("\n");  // collection
-    let firstLine = true;
     const result = [];
-    const loopItems = lines;
+    const loopItems = lines.slice(1);
 
     for (const line of loopItems) {
-        if (firstLine) {
-            firstLine = false;
-            continue;
-        }
         if (line.trim() === "") continue;
         const record = line.split(",");
         if (record[1].trim() === "India") {
