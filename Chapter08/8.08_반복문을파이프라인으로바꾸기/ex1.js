90574173b5ff184eeec95e3295ad82d4dec28b77
Juhye-Kim <kjh960215@gmail.com>
2022-04-16 23:38:57 +0900

    ch08-8 ex1 map으로 대체

--- Chapter08/8.08_반복문을파이프라인으로바꾸기/ex1.js
+++ Chapter08/8.08_반복문을파이프라인으로바꾸기/ex1.js
@@ -1,10 +1,10 @@
 function acquireData(input) {
     const lines = input.split("\n");  // collection
     const result = [];
-    const loopItems = lines.slice(1).filter(line => line.trim() !== "");
+    const loopItems = lines.slice(1).filter(line => line.trim() !== "").map(line => line.split(","));
 
     for (const line of loopItems) {
-        const record = line.split(",");
+        const record = line;
         if (record[1].trim() === "India") {
             result.push({city: record[0].trim(), phone: record[2].trim()});
         }
