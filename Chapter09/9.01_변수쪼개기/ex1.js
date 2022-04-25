07653fef4c471e9cc562efc89b5562fc32b356dc
Juhye-Kim <kjh960215@gmail.com>
2022-04-25 21:29:40 +0900

    ch09-1 ex1 변수명 새로 짓고 대입하기

--- Chapter09/9.01_변수쪼개기/ex1.js
+++ Chapter09/9.01_변수쪼개기/ex1.js
@@ -6,14 +6,14 @@
  */
 function distanceTravelled(scenario, time) {
     let result;
-    let acc = scenario.primaryForce / scenario.mass;  // a = F/m  -> 첫번째 힘이 유발한 초기 가속도
+    const primaryAcceleration = scenario.primaryForce / scenario.mass;  // a = F/m  -> 첫번째 힘이 유발한 초기 가속도
     let primaryTime = Math.min(time, scenario.delay);
-    result = 0.5 * acc * primaryTime * primaryTime;  // 전파된 거리
+    result = 0.5 * primaryAcceleration * primaryTime * primaryTime;  // 전파된 거리
     let secondaryTime = time - scenario.delay;
 
     if (secondaryTime > 0) {  // 두번째 힘 반영해 다시 계산
-        let primaryVelocity = acc * scenario.delay;
-        acc = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;  // 두번째 힘까지 반영된 후의 가속도
+        let primaryVelocity = primaryAcceleration * scenario.delay;
+        let acc = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;  // 두번째 힘까지 반영된 후의 가속도
         result += primaryVelocity * secondaryTime + 0.5 * acc * secondaryTime * secondaryTime;
     }
 
