f3ab63bdbc646a32b37e802c925becdcdafcfca8
Juhye-Kim <kjh960215@gmail.com>
2022-04-25 21:43:03 +0900

    ch09-2 ex1 클래스로 캡슐화

--- Chapter09/9.02_필드이름바꾸기/ex1.js
+++ Chapter09/9.02_필드이름바꾸기/ex1.js
@@ -1,1 +1,24 @@
-const organization = {name: 'juhye', country: 'KR'};
+class Organization {
+    constructor(data) {
+        this._name = data.name;
+        this._country = data.country;
+    }
+    
+    get name() {
+        return this._name;
+    }
+    
+    set name(aString) {
+        this._name = aString;
+    }
+    
+    get country() {
+        return this._country;
+    }
+    
+    set country(aCountryCode) {
+        this._country = aCountryCode;
+    }
+}
+
+const organization = new Organization({name: 'juhye', country: 'KR'});
