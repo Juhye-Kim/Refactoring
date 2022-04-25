ec665c3ea7bc3ad1d259162e52a96c86f2897037
Juhye-Kim <kjh960215@gmail.com>
2022-04-25 21:33:04 +0900

    ch09-1 ex2 변수명 바꾸기

--- Chapter09/9.01_변수쪼개기/ex2.js
+++ Chapter09/9.01_변수쪼개기/ex2.js
@@ -1,9 +1,9 @@
 // 입력 매개변수 값을 수정할 때
 function discount(originalInputValue, quantity) {
-    let inputValue = originalInputValue;
+    let result = originalInputValue;
 
-    if (inputValue > 50) inputValue -= 2;
-    if (quantity > 100) inputValue -= 1;
+    if (inputValue > 50) result -= 2;
+    if (quantity > 100) result -= 1;
 
-    return inputValue;
+    return result;
 }
