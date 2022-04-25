3b0a15044070b4f5467043114bd6f48322cd93f7
Juhye-Kim <kjh960215@gmail.com>
2022-04-25 21:32:36 +0900

    ch09-1 ex2 inputValue 쪼개기

--- Chapter09/9.01_변수쪼개기/ex2.js
+++ Chapter09/9.01_변수쪼개기/ex2.js
@@ -1,4 +1,7 @@
-function discount(inputValue, quantity) {
+// 입력 매개변수 값을 수정할 때
+function discount(originalInputValue, quantity) {
+    let inputValue = originalInputValue;
+
     if (inputValue > 50) inputValue -= 2;
     if (quantity > 100) inputValue -= 1;
 
