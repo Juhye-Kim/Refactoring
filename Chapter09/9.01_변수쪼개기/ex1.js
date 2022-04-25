856c7022ba8d41eb2094d6d19878ded2fa127c45
Juhye-Kim <kjh960215@gmail.com>
2022-04-25 21:26:45 +0900

    ch09-1 ex1 before

---
+++ Chapter09/9.01_변수쪼개기/ex1.js
@@ -0,0 +1,21 @@
+/**
+ * 음식이 다른 지역으로 전파된 거리를 구하는 함수
+ * @param {object} scenario 
+ * @param {number} time 
+ * @returns {number} 
+ */
+function distanceTravelled(scenario, time) {
+    let result;
+    let acc = scenario.primaryForce / scenario.mass;  // a = F/m  -> 첫번째 힘이 유발한 초기 가속도
+    let primaryTime = Math.min(time, scenario.delay);
+    result = 0.5 * acc * primaryTime * primaryTime;  // 전파된 거리
+    let secondaryTime = time - scenario.delay;
+
+    if (secondaryTime > 0) {  // 두번째 힘 반영해 다시 계산
+        let primaryVelocity = acc * scenario.delay;
+        acc = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;  // 두번째 힘까지 반영된 후의 가속도
+        result += primaryVelocity * secondaryTime + 0.5 * acc * secondaryTime * secondaryTime;
+    }
+
+    return result;
+}
