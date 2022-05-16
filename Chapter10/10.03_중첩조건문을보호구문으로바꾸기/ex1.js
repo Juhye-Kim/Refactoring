3a63e0197696b3f30932e3340b1b00c38d357ca7
Juhye-Kim <kjh960215@gmail.com>
2022-05-16 21:28:52 +0900

    ch10-3: ex1 보호구문으로 바꾸기(1)

--- Chapter10/10.03_중첩조건문을보호구문으로바꾸기/ex1.js
+++ Chapter10/10.03_중첩조건문을보호구문으로바꾸기/ex1.js
@@ -1,19 +1,16 @@
 function getPayAmount(employee) {
     let result;
 
-    if (employee.isSeparated) {
-        result = {amount: 0, reasonCode: "SEP"};
+    if (employee.isSeparated) return {amount: 0, reasonCode: "SEP"};
+    if (employee.isRetired) {
+        result = {amount: 0, reasonCode: "RET"};
     } else {
-        if (employee.isRetired) {
-            result = {amount: 0, reasonCode: "RET"};
-        } else {
-            // 급여 계산 로직
-            lorem.ipsum(dolor.sitAmet);
-            consectetur(adipiscing).elit();
-            sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
-            ut.enim.ad(minim.veniam);
-            result = someFinalComputation();
-        }
+        // 급여 계산 로직
+        lorem.ipsum(dolor.sitAmet);
+        consectetur(adipiscing).elit();
+        sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
+        ut.enim.ad(minim.veniam);
+        result = someFinalComputation();
     }
 
     return result;
