e66d1a49c05591453a5d6781200e748b6ac60003
Juhye-Kim <kjh960215@gmail.com>
2022-05-16 21:31:37 +0900

    ch10-3: ex2 before

---
+++ Chapter10/10.03_중첩조건문을보호구문으로바꾸기/ex2.js
@@ -0,0 +1,12 @@
+// 예시2: 조건 반대로 만들기
+function adjustedCapital(anInstrument) {
+    let result = 0;
+
+    if (anInstrument.capital > 0) {
+        if (anInstrument.interestRate > 0 && anInstrument.duration > 0) {
+            result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
+        }
+    }
+
+    return result;
+}
