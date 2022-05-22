27b7de3dbfef03582f82984577baafefa8718b06
Juhye-Kim <kjh960215@gmail.com>
2022-05-22 19:58:53 +0900

    ch10-4: ex2 여러 함수를 클래스로 묶기

--- Chapter10/10.04_조건문로직을다형성으로바꾸기/ex2.js
+++ Chapter10/10.04_조건문로직을다형성으로바꾸기/ex2.js
@@ -10,76 +10,84 @@
      {zone: "아프리카", profit: 7},
  ];
 
- const myRating = rating(voyage, history);
+ const myRating = new Rating(voyage, history).value;
 
-/**
- * 두 가지 요소를 종합해 항해의 최종 등급을 계산
- * - 위험 요소 : voyageRisk, captainHistoryRisk
- * - 잠재 수익 : voyageProfitFactor
- */
-function rating(voyage, history) {
-    const vpf = voyageProfitFactor(voyage, history);
-    const vr = voyageRisk(voyage);
-    const chr = captainHistoryRisk(voyage, history);
-
-    if (vpf * 3 > (vr + chr * 2)) return "A";
-    else return "B";
-}
-
-/**
- * 항해경로 위험요소
- */
-function voyageRisk(voyage) {
-    let result = 1;
-
-    if (voyage.length > 4) result += 2;
-    if (voyage.length > 8) result += voyage.length - 8;
-    if (["중국", "동인도"].includes(voyage.zone)) result += 4;
-
-    return Math.max(result, 0);
-}
-
-/**
- * 선장의 항해 이력 위험요소
- */
-function captainHistoryRisk(voyage, history) {
-    let result = 1;
-
-    if (history.length < 5) result += 4;
-
-    result += history.filter(v => v.profit < 0).length;
-
-    if (voyage.zone === "중국" && hasChina(history)) result -= 2;
-
-    return Math.max(result, 0);
-}
-
-/**
- * 중국 경유 여부
- */
-function hasChina(history) {
-    return history.some(v => "중국" === v.zone);
-}
-
-/**
- * 수익 요인
- */
-function voyageProfitFactor(voyage, history) {
-    let result = 2;
-    
-    if (voyage.zone === "중국") result += 1;
-    if (voyage.zone === "동인도") result += 1;
-    if (voyage.zone === "중국" && hasChina(history)) {
-        result += 3;
-
-        if (history.length > 10) result += 1;
-        if (voyage.length > 12) result += 1;
-        if (voyage.length > 18) result -= 1;
-    } 
-    else {
-        if (history.length > 8) result += 1;
-        if (voyage.length > 14) result -= 1;
+ class Rating {
+    constructor(voyage, history) {
+        this._voyage = voyage;
+        this._history = history;
     }
 
-    return result;
-}
+    /**
+     * 두 가지 요소를 종합해 항해의 최종 등급을 계산
+     * - 위험 요소 : voyageRisk, captainHistoryRisk
+     * - 잠재 수익 : voyageProfitFactor
+     */
+    get value() {
+        const vpf = voyageProfitFactor(voyage, history);
+        const vr = voyageRisk(voyage);
+        const chr = captainHistoryRisk(voyage, history);
+    
+        if (vpf * 3 > (vr + chr * 2)) return "A";
+        else return "B";
+    }
+
+    /**
+     * 항해경로 위험요소
+     */
+    get voyageRisk() {
+        let result = 1;
+
+        if (this._voyage.length > 4) result += 2;
+        if (this._voyage.length > 8) result += this._voyage.length - 8;
+        if (["중국", "동인도"].includes(this._voyage.zone)) result += 4;
+
+        return Math.max(result, 0);
+    }
+
+    /**
+     * 선장의 항해 이력 위험요소
+     */
+    get captainHistoryRisk() {
+        let result = 1;
+
+        if (this._history.length < 5) result += 4;
+
+        result += this._history.filter(v => v.profit < 0).length;
+
+        if (this._voyage.zone === "중국" && this.hasChinaHistory) result -= 2;
+
+        return Math.max(result, 0);
+    }
+
+    /**
+     * 중국 경유 여부
+     */
+    get hasChinaHistory() {
+        return this._history.some(v => "중국" === v.zone);
+    }
+
+    /**
+     * 수익 요인
+     */
+    get voyageProfitFactor() {
+        let result = 2;
+        
+        if (this._voyage.zone === "중국") result += 1;
+        if (this._voyage.zone === "동인도") result += 1;
+        if (this._voyage.zone === "중국" && this.hasChinaHistory) {
+            result += 3;
+
+            if (this._this._history.length > 10) result += 1;
+            if (this._voyage.length > 12) result += 1;
+            if (this._voyage.length > 18) result -= 1;
+        } 
+        else {
+            if (this._this._history.length > 8) result += 1;
+            if (this._voyage.length > 14) result -= 1;
+        }
+
+        return result;
+    }
+
+ }
