03d529eb7043c7e18ce51d68bbb16a7641ac2bbb
Juhye-Kim <kjh960215@gmail.com>
2022-05-22 19:27:37 +0900

    ch10-4: ex1 여러 함수를 클래스로 묶기

--- Chapter10/10.04_조건문로직을다형성으로바꾸기/ex1.js
+++ Chapter10/10.04_조건문로직을다형성으로바꾸기/ex1.js
@@ -15,16 +15,7 @@ function speeds(birds) {
  * @returns {string} plumage
  */
 function plumage(bird) {
-    switch (bird.type) {
-        case '유럽 제비':
-            return "보통이다";
-        case '아프리카 제비':
-            return (bird.numberOfCoCounts > 2) ? "지쳤다" : "보통이다";
-        case '노르웨이 파랑 앵무':
-            return (bird.voltage > 100) ? "그을렸다" : "예쁘다";
-        default:
-            return "알 수 없다";
-    }
+    return new Bird(bird).plumage;
 }
 
 /**
@@ -33,14 +24,37 @@ function plumage(bird) {
  * @returns {number | null} speed 
  */
 function airSpeedVelocity(bird) {
-    switch (bird.type) {
-        case '유럽 제비':
-            return 35;
-        case '아프리카 제비':
-            return 40 - 2 * bird.numberOfCoCounts;
-        case '노르웨이 파랑 앵무':
-            return (bird.isNailed) ? 0 : 10 + bird.voltage / 10;
-        default:
-            return null;
+    return new Bird(bird).airSpeedVelocity;
+}
+
+class Bird {
+    constructor(birdObject) {
+        Object.assign(this, birdObject);
+    }
+
+    get plumage() {
+        switch (bird.type) {
+            case '유럽 제비':
+                return "보통이다";
+            case '아프리카 제비':
+                return (bird.numberOfCoCounts > 2) ? "지쳤다" : "보통이다";
+            case '노르웨이 파랑 앵무':
+                return (bird.voltage > 100) ? "그을렸다" : "예쁘다";
+            default:
+                return "알 수 없다";
+        }
+    }
+
+    get airSpeedVelocity() {
+        switch (bird.type) {
+            case '유럽 제비':
+                return 35;
+            case '아프리카 제비':
+                return 40 - 2 * bird.numberOfCoCounts;
+            case '노르웨이 파랑 앵무':
+                return (bird.isNailed) ? 0 : 10 + bird.voltage / 10;
+            default:
+                return null;
+        }
     }
 }
