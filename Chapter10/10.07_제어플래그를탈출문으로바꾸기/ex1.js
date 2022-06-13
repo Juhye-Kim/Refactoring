da1d8b2fd010661bae80b7d3414386d21d4dcf83
Juhye-Kim <kjh960215@gmail.com>
2022-06-13 21:23:37 +0900

    ch10-7: ex1 더 가다듬기

--- Chapter10/10.07_제어플래그를탈출문으로바꾸기/ex1.js
+++ Chapter10/10.07_제어플래그를탈출문으로바꾸기/ex1.js
@@ -1,14 +1,5 @@
 function checkForMiscreants(people) {
-    for (const p of people) {
-        if (p === "조커") {
-            sendAlert();
-            return;
-        }
-        if (p === "사루만") {
-            sendAlert();
-            return;
-        }
-    }
+    if (people.some(p => ["조커", "사루만"].includes(p))) sendAlert();
 }
 
 checkForMiscreants(people);
