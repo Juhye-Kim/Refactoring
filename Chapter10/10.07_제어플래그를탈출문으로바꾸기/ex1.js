0e9088dbca144ae4b8e74c8982602c2d5c978583
Juhye-Kim <kjh960215@gmail.com>
2022-06-13 21:22:41 +0900

    ch10-7: ex1 제어 플래그 제거

--- Chapter10/10.07_제어플래그를탈출문으로바꾸기/ex1.js
+++ Chapter10/10.07_제어플래그를탈출문으로바꾸기/ex1.js
@@ -1,16 +1,12 @@
 function checkForMiscreants(people) {
-    let found = false;
-    
     for (const p of people) {
-        if (!found) {
-            if (p === "조커") {
-                sendAlert();
-                found = true;
-            }
-            if (p === "사루만") {
-                sendAlert();
-                found = true; 
-            }
+        if (p === "조커") {
+            sendAlert();
+            return;
+        }
+        if (p === "사루만") {
+            sendAlert();
+            return;
         }
     }
 }
