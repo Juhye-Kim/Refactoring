096e76dfc412a95475e70def12885bfdc797cda2
Juhye-Kim <kjh960215@gmail.com>
2022-06-13 20:02:56 +0900

    ch10-5: ex1 before

---
+++ Chapter10/10.05_특이케이스추가하기/ex1.js
@@ -0,0 +1,39 @@
+class Site {
+    get customer() {
+        return this._customer;
+    }
+}
+
+class Customer {
+    get name() {
+        return this._name;
+    }
+
+    get billingPlan() {
+        return this._billingPlan;  // 요금제
+    }
+
+    set billingPlan(args) {
+        this._billingPlan = args;
+    }
+
+    get paymentHistory() {
+        return this._paymentHistory;  // 납부 이력
+    }
+}
+
+// client1
+const aCustomer = site.customer;
+
+let customerName;
+if (aCustomer === "미확인 고객") customerName = "거주자";
+else customerName = aCustomer.name;
+
+// client2
+const plan = (aCustomer === "미확인 고객") ? registry.billingPlans.basic : aCustomer.billingPlan;
+
+// client3
+if (aCustomer !== "미확인 고객") aCustomer.billingPlan = newPlan;
+
+// client4
+const weeksDelinquent = (aCustomer === "미확인 고객") ? 0 : aCustomer.paymentHistory.weeksDelinquentInLastYear;
