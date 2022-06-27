052113cf607d28da10ce6a2bd5a84ce067c9efdd
Juhye-Kim <kjh960215@gmail.com>
2022-06-27 17:42:31 +0900

    ch11-2: ex1 함수를 호출하는 곳 수정

--- Chapter11/11.02_함수_매개변수화하기/ex1.js
+++ Chapter11/11.02_함수_매개변수화하기/ex1.js
@@ -1,19 +1,11 @@
 function baseCharge(usage) {
     if (usage < 0) return usd(0);
 
-    const amount = bottomBand(usage) * 0.03 + withinBand(usage, 100, 200) * 0.05 + topBand(usage) * 0.07;
+    const amount = withinBand(usage, 0, 100) * 0.03 + withinBand(usage, 100, 200) * 0.05 + topBand(usage, 200, Infinity) * 0.07;
 
     return usd(amount);
 }
 
-function bottomBand(usage) {
-    return Math.min(usage, 100);
-}
-
 function withinBand(usage, bottom, top) {
     return usage > bottom ? Math.min(usage, top) - bottom : 0;
-}
-
-function topBand(usage) {
-    return usage > 200 ? usage - 200 : 0;
 }
