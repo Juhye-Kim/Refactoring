fd4663a5a8e0bcddfd1346726d7517ddea5141a9
Juhye-Kim <kjh960215@gmail.com>
2022-06-27 17:19:39 +0900

    ch11-1: ex1 before

---
+++ Chapter11/11.01_질의함수와_변경함수_분리하기/ex1.js
@@ -0,0 +1,13 @@
+function alertForMiscreant(people) {
+    for (const p of people) {
+        if (p === "조커") {
+            setOffAlarms();
+            return "조커";
+        }
+        if (p === "사루만") {
+            setOffAlarms();
+            return "사루만";
+        }
+    }
+    return "";
+}
