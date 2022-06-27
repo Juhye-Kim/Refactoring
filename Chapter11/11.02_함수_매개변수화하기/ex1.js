dfcf709b466272371eaa95d8e7f8da8ecb66b4be
Juhye-Kim <kjh960215@gmail.com>
2022-06-27 17:41:33 +0900

    ch11-2: ex1 함수 하나를 선택해 함수 선언 바꾸기로 리터럴들을 매개변수로 추가

--- Chapter11/11.02_함수_매개변수화하기/ex1.js
+++ Chapter11/11.02_함수_매개변수화하기/ex1.js
@@ -1,7 +1,7 @@
 function baseCharge(usage) {
     if (usage < 0) return usd(0);
 
-    const amount = bottomBand(usage) * 0.03 + middleBand(usage) * 0.05 + topBand(usage) * 0.07;
+    const amount = bottomBand(usage) * 0.03 + withinBand(usage, 100, 200) * 0.05 + topBand(usage) * 0.07;
 
     return usd(amount);
 }
@@ -10,8 +10,8 @@ function bottomBand(usage) {
     return Math.min(usage, 100);
 }
 
-function middleBand(usage) {
-    return usage > 100 ? Math.min(usage, 200) - 100 : 0;
+function withinBand(usage, bottom, top) {
+    return usage > bottom ? Math.min(usage, top) - bottom : 0;
 }
 
 function topBand(usage) {
