8f891346a7de9f00998cb154860cd4e575fc0c46
Juhye-Kim <kjh960215@gmail.com>
2022-06-27 17:20:51 +0900

    ch11-1: ex1 대상함수를 복제하고 새 질의 함수에서 side effect 제거

--- Chapter11/11.01_질의함수와_변경함수_분리하기/ex1.js
+++ Chapter11/11.01_질의함수와_변경함수_분리하기/ex1.js
@@ -11,3 +11,15 @@ function alertForMiscreant(people) {
     }
     return "";
 }
+
+function findMiscreant(people) {
+    for (const p of people) {
+        if (p === "조커") {
+            return "조커";
+        }
+        if (p === "사루만") {
+            return "사루만";
+        }
+    }
+    return "";  
+}
